4f5b1b0fd2d6f44da2e84137a4bd5aef1fa0c456
james18178 <james18178@163.com>
2024-10-29 20:09:23 +0800

    改了一些bug;重点更新-支持中文

--- core-build/src/main.cpp
+++ core-build/src/main.cpp
@@ -1,27 +1,30 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 
 #include <opencv2/core/version.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 
+#include "CvxFont.h"
+using namespace cvx;
+
 #include <emscripten.h>
 #include <emscripten/bind.h>
 
-
 using namespace emscripten;
 
-static const std::string version = "miaowuWaterMark-release-v1.0.0";
+static const std::string version = "miaoWM-release-v1.0.0";
 
 static const std::string opencv_version = CV_VERSION;
 
 // 声明
 std::string base64_encode(unsigned char const* bytes_to_encode, size_t in_len, bool url = false);
-cv::Mat simple_watermark_lib(const std::string& image_path, const std::string& text, const cv::Scalar& font_color);
-cv::Mat full_screen_watermark_lib(const std::string& image_path, const std::string& text, const cv::Scalar& font_color);
+cv::Mat simple_watermark_lib(cvx::CvxFont& font_ttf, const std::string& image_path, const std::string& text, const cv::Scalar& font_color);
+cv::Mat full_screen_watermark_lib(cvx::CvxFont& font_ttf, const std::string& image_path, const std::string& text, const cv::Scalar& font_color);
 bool can_place_text(int x, int y, const cv::Mat& occupied, const cv::Size& text_size, const cv::Mat& edges);
 void occupy_region(int x, int y, cv::Mat& occupied, const cv::Size& text_size);
-cv::Mat edge_watermark_lib(const std::string& image_path, const std::string& text, const cv::Scalar& font_color, double gain);
+cv::Mat edge_watermark_lib(cvx::CvxFont& font_ttf, const std::string& image_path, const std::string& text, const cv::Scalar& font_color, double gain, int font_size);
 std::string cvobj_to_base64(const cv::Mat& opencv_img);
 cv::Scalar string_to_scalar(const std::string& input);
 
@@ -80,13 +83,14 @@ std::string base64_encode(unsigned char const* bytes_to_encode, size_t in_len, b
 
 // 简单水印
 // void simple_watermark(const std::string& image_path, const std::string& output_path, const std::string& text, const cv::Scalar& font_color = cv::Scalar(255, 255, 255)) {
-cv::Mat simple_watermark_lib(const std::string& image_path, const std::string& text, const cv::Scalar& font_color = cv::Scalar(255, 255, 255)) {
+cv::Mat simple_watermark_lib(cvx::CvxFont& font_ttf, const std::string& image_path, const std::string& text, const cv::Scalar& font_color = cv::Scalar(255, 255, 255)) {
     cv::Mat img = cv::imread(image_path, cv::IMREAD_UNCHANGED);
     int h = img.rows;
     int w = img.cols;
     cv::Mat layer(h, w, CV_8UC4, cv::Scalar(0, 0, 0, 0));
     cv::Point text_position(50, h - 50);
-    cv::putText(layer, text, text_position, cv::FONT_HERSHEY_SIMPLEX, 2, font_color, 6, cv::LINE_AA);
+    cvx::putText(layer, text, text_position, font_ttf, 240, font_color);
+    // cv::putText(layer, text, text_position, cv::FONT_HERSHEY_SIMPLEX, 2, font_color, 6, cv::LINE_AA);
     cv::cvtColor(img, img, cv::COLOR_BGR2BGRA);
     cv::Mat result;
     cv::addWeighted(img, 1.0, layer, 1.0, 0, result);
@@ -96,7 +100,7 @@ cv::Mat simple_watermark_lib(const std::string& image_path, const std::string& t
 
 // 全屏水印
 // void full_screen_watermark(const std::string& image_path, const std::string& output_path, const std::string& text, const cv::Scalar& font_color = cv::Scalar(255, 255, 255)) {
-cv::Mat full_screen_watermark_lib(const std::string& image_path, const std::string& text, const cv::Scalar& font_color = cv::Scalar(255, 255, 255)) {
+cv::Mat full_screen_watermark_lib(cvx::CvxFont& font_ttf, const std::string& image_path, const std::string& text, const cv::Scalar& font_color = cv::Scalar(255, 255, 255)) {
     cv::Mat img = cv::imread(image_path, cv::IMREAD_UNCHANGED);
     int h = img.rows;
     int w = img.cols;
@@ -104,19 +108,18 @@ cv::Mat full_screen_watermark_lib(const std::string& image_path, const std::stri
     cv::Mat layer(h, w, CV_8UC4, cv::Scalar(0, 0, 0, 0));
     cv::Point text_position;
     cv::Scalar bg_color(0, 0, 0, 0);
-    for (int w_s = 0; w_s < w / text.length(); w_s++) {
-        if (w_s % 2 == 0) {
-            a_w += text.length() * 13;
-            continue;
+    // int text_len = text.length();
+
+    int text_width = text.length() * 20;
+    int text_height = 36;
+
+    for (int y = text_height; y < h; y += text_height + 20) {
+        for (int x = 0; x < w; x += text_width + 20) {
+            // cv::putText(watermark_layer, watermark_text, cv::Point(x, y), font, font_scale, text_color, font_thickness);
+            cvx::putText(layer, text, cv::Point(x, y), font_ttf, text_height, font_color);
         }
-        int a_h = 50;
-        for (int i = 0; i < h / 2; i++) {
-            text_position = cv::Point(a_w, a_h);
-            cv::putText(layer, text, text_position, cv::FONT_HERSHEY_SIMPLEX, 0.5, font_color, 1, cv::LINE_AA);
-            a_h += 20;
-        }
-        a_w += text.length() * 13;
     }
+
     cv::cvtColor(img, img, cv::COLOR_BGR2BGRA);
     cv::Mat result;
     cv::addWeighted(img, 1.0, layer, 1.0, 0, result);
@@ -148,7 +151,7 @@ void occupy_region(int x, int y, cv::Mat& occupied, const cv::Size& text_size) {
 
 // 边缘水印
 // void edge_watermark(const std::string& image_path, const std::string& output_path, const std::string& text, const cv::Scalar& font_color = cv::Scalar(255, 255, 255), double gain = 0.2) {
-cv::Mat edge_watermark_lib(const std::string& image_path, const std::string& text, const cv::Scalar& font_color = cv::Scalar(255, 255, 255), double gain = 0.2) {
+cv::Mat edge_watermark_lib(cvx::CvxFont& font_ttf, const std::string& image_path, const std::string& text, const cv::Scalar& font_color = cv::Scalar(255, 255, 255), double gain = 0.2, int font_size = 12) {
     cv::Mat image = cv::imread(image_path, cv::IMREAD_UNCHANGED);
     int h = image.rows;
     int w = image.cols;
@@ -159,12 +162,13 @@ cv::Mat edge_watermark_lib(const std::string& image_path, const std::string& tex
     cv::Mat occupied = cv::Mat::zeros(edges.size(), CV_8UC1);
     cv::Mat layer(h, w, CV_8UC4, cv::Scalar(0, 0, 0, 0));
     cv::Point text_position;
-    cv::Size text_size(7 * text.length(), 10);
+    cv::Size text_size(7 + text.length(), font_size);
     for (int y = 0; y < edges.rows; y++) {
         for (int x = 0; x < edges.cols; x++) {
             if (edges.at<uchar>(y, x) != 0 && can_place_text(x, y, occupied, text_size, edges)) {
                 text_position = cv::Point(x, y);
-                cv::putText(layer, text, text_position, cv::FONT_HERSHEY_SIMPLEX, 0.5, font_color, 1, cv::LINE_AA);
+                // cv::putText(layer, text, text_position, cv::FONT_HERSHEY_SIMPLEX, 0.5, font_color, 1, cv::LINE_AA);
+                cvx::putText(layer, text, text_position, font_ttf, font_size, font_color);
                 occupy_region(x, y, occupied, text_size);
             }
         }
@@ -186,6 +190,11 @@ std::string cvobj_to_base64(const cv::Mat& opencv_img) {
     return base64_encode(buffer.data(), buffer.size());
 }
 
+// test output
+// void cvobj_save(const cv::Mat& opencv_img) {
+//     cv::imwrite("output.png", opencv_img);
+// }
+
 cv::Scalar string_to_scalar(const std::string& input) {
     std::istringstream iss(input);
     std::string token;
@@ -201,7 +210,7 @@ cv::Scalar string_to_scalar(const std::string& input) {
         }
     }
     try {
-        return cv::Scalar(values[0], values[1], values[2]);
+        return cv::Scalar(values[2], values[1], values[0]); // 哈哈 您猜怎么着 RGB读入顺序反了
     } catch (const std::exception& e) { 
         return false;
     }
@@ -209,18 +218,23 @@ cv::Scalar string_to_scalar(const std::string& input) {
 }
 
 // wasm 对外扩展
-std::string simple_watermark_main(const std::string& fileName, const std::string& text, const std::string& colorData) {
-    std::string result = cvobj_to_base64(simple_watermark_lib(fileName, text, string_to_scalar(colorData)));
+std::string simple_watermark_main(const std::string& ttfFileName, const std::string& fileName, const std::string& text, const std::string& colorData) {
+    cvx::CvxFont font_ttf(ttfFileName);
+    std::string result = cvobj_to_base64(simple_watermark_lib(font_ttf, fileName, text, string_to_scalar(colorData)));
     return result;
 }
 
-std::string full_screen_watermark_main(const std::string& fileName, const std::string& text, const std::string& colorData) {
-    std::string result = cvobj_to_base64(full_screen_watermark_lib(fileName, text, string_to_scalar(colorData)));
+std::string full_screen_watermark_main(const std::string& ttfFileName, const std::string& fileName, const std::string& text, const std::string& colorData) {
+   cvx::CvxFont font_ttf(ttfFileName);
+   std::string result = cvobj_to_base64(full_screen_watermark_lib(font_ttf, fileName, text, string_to_scalar(colorData)));
     return result;
 }
 
-std::string edge_watermark_main(const std::string& fileName, const std::string& text, const std::string& colorData, double gain) {
-    std::string result = cvobj_to_base64(edge_watermark_lib(fileName, text, string_to_scalar(colorData), gain));
+std::string edge_watermark_main(const std::string& ttfFileName, const std::string& fileName, const std::string& text, const std::string& colorData, double gain, int font_size_percent) { 
+    cvx::CvxFont font_ttf(ttfFileName);
+    // font_size 10 - 20
+    int font_size = static_cast<int>(std::round(10 + (font_size_percent / 100.0) * (20 - 10)));
+    std::string result = cvobj_to_base64(edge_watermark_lib(font_ttf, fileName, text, string_to_scalar(colorData), gain, font_size));
     return result;
 }
 
@@ -232,3 +246,9 @@ EMSCRIPTEN_BINDINGS(recognizer)
     function("full_screen_watermark", &full_screen_watermark_main);
     function("edge_watermark", &edge_watermark_main);
 }
+
+// int main() {
+//     edge_watermark_main("/app/a.ttf" ,"/app/test.jpg", "你好", "255,0,0", 1.0, 100);
+//     std::cout << opencv_version << std::endl;
+//     return 0;
+// }
