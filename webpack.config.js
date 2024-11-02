130f0caf932b26ef2dab2292c51d802d5531f2c0
James <44807803+James18178@users.noreply.github.com>
2024-11-02 10:20:37 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,9 @@ module.exports = {
   mode: 'production',
   entry: './src/main.js',
   output: {
-    filename: 'miaoWM.js',
+    filename: 'miaoWM.worker.js',
     path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'commonjs',
-    umdNamedDefine: true,
+    globalObject: 'this'
   },
   resolve: {
     fallback: {
